dcb71bc1cfa36d83c051a775fa7440710ab9545d
Shobhit Tyagi <Shobhit.tya@gmail.com>
2018-04-01 15:40:52 +0530

    Refactor API

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore'); 
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
 
-var todonextId = 1;
+var todoNextId = 1;
 
 app.use(bodyParser.json());
 
@@ -18,41 +20,31 @@ app.get('/todos', function(req, res){
 })
 // GET  /todos/:id
 app.get('/todos/:id', function(req, res)
-{
+{      var todoID = parseInt(req.params.id, 10);
+         var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    var todoID = parseInt(req.params.id, 10);
-    var matchedTodo;
-     todos.forEach (function (todo)
-     {
-         if(todoID === todo.id) 
-         {
-             matchedTodo = todo;
-         }
-     });
-
-     if (matchedTodo)
-    {
+     if (matchedTodo) {
          res.json(matchedTodo);
-
-     } else
-     {
+     } else {
          res.status(404).send();
      }
-    
-
-});
+ });
 
 // // POST /todos
 app.post('/todos', function(req, res){
-    var body=req.body;
+    var body= _.pick(req.body, 'description', 'completed');
 
-    body.id = todonextId++;
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
 
-    todos.push(body);console.log('Description' + body.description);
+    body.description = body.description.trim();
+    body.id = todoNextId++;
+
+    todos.push(body);
+
     res.json(body);
 });
-
-
 app.listen(PORT, function(){
     console.log('Express listening on port ' + ' ' + PORT + '!');
 });
